2020dfb3eda31929478d9c7efc417ba898d6b7d1
Italo Silva <italo@maleldil.com>
2022-01-11 14:08:59 -0300

    echo commands

--- python/run_test.sh
+++ python/run_test.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
+set -x
+
 echo '# Installing module'
 python3 -m venv .venv
+set +x
 source .venv/bin/activate
+set -x
 pip install -r requirements.txt
 echo -e '\n# Testing module'
 python test_stub.py
--- rust/run_test.sh
+++ rust/run_test.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
+set -x
+
 python3 -m venv .venv
+set +x
 source .venv/bin/activate
+set -x
 
 echo '# Building module'
 maturin develop
